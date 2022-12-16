57db5b549de95e720c9cc7bc47d5141c722952db
rafaelgscc <37811294+rafaelgscc@users.noreply.github.com>
2022-12-16 10:27:09 -0300

    Modificado item #1

--- automatizamacro.py
+++ automatizamacro.py
@@ -8,11 +8,7 @@ import win32com.client, win32api
 # Data: 03/11/2022                          #
 #############################################
 
-#1) baixando os arquivos do e-mail
-
-
-
-#2) Executar o macro
+#1) Executar o macro
 
 print("Executando o MACRO")
 win32api.MessageBox(0, 'Executando o Macro', 'Script')
@@ -26,6 +22,7 @@ if os.path.exists("Macro_PLOAWEB_V49.xlsm"):
     del plan
     sys.exit(0)
 
-#3) Mover os arquivos para o Z
+#2) Mover os arquivos para o Z
+##WorkAround: Fazer testes com movimento de arquivos
 
 
